73f597cdad1aa84f7b7661749899fd69a7589a1a
git <lipusipupara@gmail.com>
2015-06-17 23:44:47 +0530

    custom page factory added to this page

---
+++ src/java/com/bnkar/core/CustomPageFactory.java
@@ -0,0 +1,11 @@
+package bnkar.core;
+
+import org.openqa.selenium.support.PageFactory;
+
+public class CustomPageFactory extends PageFactory {
+
+public CustomPageFactory(){
+	super();
+}
+
+}
--- test/com/bnkar/locator/Testgoogle.java
+++ test/com/bnkar/locator/Testgoogle.java
@@ -4,12 +4,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
 
+import bnkar.core.CustomPageFactory;
+
 public class Testgoogle {
 	public static void main(String[] args) {
 		System.out.println("something");
 		WebDriver driver=new FirefoxDriver();
 		driver.get("http://www.google.com/");
-		GooglePage gp=PageFactory.initElements(driver,GooglePage.class);
+		GooglePage gp=CustomPageFactory.initElements(driver,GooglePage.class);
 		gp.searchbox.sendKeys("hello");
 	}
 }
