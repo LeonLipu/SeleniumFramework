638486d3f603b54b066614982e265f766d7514f4
git <lipusipupara@gmail.com>
2015-06-19 20:32:27 +0530

    deleted random variable to create in future

--- src/java/com/bnkar/services/DataGenerator.java
+++ src/java/com/bnkar/services/DataGenerator.java
@@ -5,31 +5,14 @@ import org.apache.commons.lang3.time.DateUtils;
 public class DataGenerator {
 
 	public static void main(String[] args) {
-		
-	}
-	
-public static String stringGenerator(String str){
-	String tempStr = String.valueOf(DateUtils
-											.getTimeinMills())
-											+ rand.nextInt((9999 - 1) + 1) + 1;
-									String uniqueString = "";
-									int len=charset.length();
-									int unilen=tempStr.length();
-									for (int i = 0; i < len; i++) {
-										char ch;
 
-										if (Character
-												.isUpperCase(charset.charAt(i))) {
-											ch = (char) (Character
-													.getNumericValue(tempStr
-															.charAt(unilen-len+i)) + 65);
-										} else {
-											ch = (char) (Character
-													.getNumericValue(tempStr
-															.charAt(unilen-len+i)) + 97);
-										}
-										uniqueString = uniqueString + ch;
-									}									
-									temp=chararr[0]+"@@"+uniqueString;
-									//System.out.println(temp);
+	}
+
+	public static String stringGenerator(String str) {
+
+		
+		
+		return str;
+
+	}
 }
