4a3e1e622e94a4bbaa1a3de3903a0d9c6b8aaa73
git <lipusipupara@gmail.com>
2015-06-28 23:37:15 +0530

    testng and pom is parameterised

--- test/com/bnkar/locator/NewTest.java
+++ test/com/bnkar/locator/NewTest.java
@@ -1,23 +1,27 @@
 package com.bnkar.locator;
 
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.AfterTest;
 
 public class NewTest {
-  @Test
-  public void f() {
-	  System.out.println("hello");
-	  System.out.println("melody id no");
-	//  System.out.println(System.getProperties());;
-	  
-  }
-  @BeforeTest
-  public void beforeTest() {
+	@Test
+  @Parameters("configPath")
+  public void f(String path) {
+  System.out.println("hello");
+  System.out.println("melody id no");
+  System.out.println("it is my print statement");
+  System.out.println(System.getProperty("exeFolder"));//mvn test -DexeFolder="something"
+	  System.out.println(path);
   }
 
-  @AfterTest
-  public void afterTest() {
-  }
+	@BeforeTest
+	public void beforeTest() {
+	}
+
+	@AfterTest
+	public void afterTest() {
+	}
 
 }
